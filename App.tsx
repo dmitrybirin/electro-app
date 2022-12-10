2937d5637f8cf7031c2000184f75871ef1065ebe
Dmitry Birin <dmitry.birin@gmail.com>
2022-12-10 15:40:21 +0200

    improving styling for the graph

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ActivityIndicator, SafeAreaView, StyleSheet, Text } from 'react-native';
+import { ActivityIndicator, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { LineChart } from 'react-native-gifted-charts';
 
 import { getSolarDataForNow } from './src/services/api';
@@ -32,7 +32,14 @@ const App = () => {
   return (
     <SafeAreaView style={styles.mainContainer}>
       <Text style={styles.mainText}>Electro</Text>
-      {loading || !data ? <ActivityIndicator /> : <LineChart data={data} />}
+      <Text style={styles.bodyText}>Planned solar for the next 24 hours</Text>
+      {loading || !data ? (
+        <ActivityIndicator />
+      ) : (
+        <View style={styles.graph}>
+          <LineChart data={data} curved={true} width={250} color1="blue" dataPointsColor1="blue" />
+        </View>
+      )}
       <Text style={styles.errorMessage}>{errorMessage}</Text>
     </SafeAreaView>
   );
@@ -48,9 +55,15 @@ const styles = StyleSheet.create({
   mainText: {
     fontSize: 24,
   },
+  bodyText: {
+    fontSize: 16,
+  },
   errorMessage: {
     fontSize: 24,
     color: 'red',
+  },
+  graph: {
+    paddingTop: 32,
   },
 });
 
