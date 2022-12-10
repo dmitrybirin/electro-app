67c0f448b485642e24f803ab8b2a22f80005703d
Dmitry Birin <dmitry.birin@gmail.com>
2022-12-10 14:19:21 +0200

    updated prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
+  printWidth: 100,
+  bracketSpacing: true,
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
 };
